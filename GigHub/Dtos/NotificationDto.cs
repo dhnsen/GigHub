64a19137b952a62ffb700b2864d89c4956a3f81c
Dan <spag1024@hotmail.com>
2019-01-05 13:23:07 -0600

    created notifications API and some DTO classes but haven't implemented them into the API yet

---
+++ GigHub/Dtos/NotificationDto.cs
@@ -0,0 +1,20 @@
+﻿using GigHub.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.Dtos
+{
+    public class NotificationDto
+    {
+        public int Id { get; private set; }
+        public DateTime DateTime { get; set; }
+        public NotificationType Type { get; private set; }
+        public DateTime? OriginalDateTime { get; private set; }
+        public string OriginalVenue { get; private set; }
+
+        public GigDto Gig { get; private set; }
+    }
+}
