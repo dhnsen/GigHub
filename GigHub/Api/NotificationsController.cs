7e5dfa111f6d2e3f8762afdc1ca7ba991b6a14a1
Dan <spag1024@hotmail.com>
2019-01-07 05:14:21 -0600

    refactored DTOs to using automatic mapping via AutoMapper plugin. I used old version 4.1.1 because i didn't know how to use 8.0

--- GigHub/Api/NotificationsController.cs
+++ GigHub/Api/NotificationsController.cs
@@ -1,4 +1,5 @@
-﻿using GigHub.Dtos;
+﻿using AutoMapper;
+using GigHub.Dtos;
 using GigHub.Models;
 using Microsoft.AspNet.Identity;
 using System;
@@ -8,7 +9,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using AutoMapper;
+
 
 namespace GigHub.Api
 {
@@ -23,38 +24,20 @@ namespace GigHub.Api
         {
             _context = new ApplicationDbContext();
         }
+        
         // GET api/<controller>
         public IEnumerable<NotificationDto> GetNewNotifications()
         {
             var userId = User.Identity.GetUserId();
             var notifications = _context.UserNotifications
-                .Where(un => un.UserId == userId)
+                .Where(un => un.UserId == userId && !un.IsRead)
                 .Select(un => un.Notification)
                 .Include(n => n.Gig.Artist)
                 .ToList();
 
             
 
-            return notifications.Select(n => new NotificationDto()
-            {
-                DateTime = n.DateTime,
-                Gig = new GigDto
-                {
-                    Artist = new UserDto
-                    {
-                        Id = n.Gig.Artist.Id,
-                        Name = n.Gig.Artist.Name
-                    },
-                    DateTime = n.Gig.DateTime,
-                    Id = n.Gig.Id,
-                    IsCanceled = n.Gig.IsCanceled,
-                    Venue = n.Gig.Venue
-                },
-                OriginalDateTime = n.OriginalDateTime,
-                OriginalVenue = n.OriginalVenue,
-                Type = n.Type
-            }
-                );
+            return notifications.Select(Mapper.Map<Notification, NotificationDto>);
         }
 
         
