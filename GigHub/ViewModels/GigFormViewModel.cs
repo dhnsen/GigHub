d457b23908dc3e7a3b8ecf708514ffdd5f87f310
Dan <spag1024@hotmail.com>
2019-02-04 07:12:36 -0600

    commented out [FutureDate] on Date peoperty of GigFormViewModel. For some reason TryPareseExact is not parsing m/d/yyyy to mm/dd/yyyy. Will fix later

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -15,7 +15,7 @@ namespace GigHub.ViewModels
         public string Venue { get; set; }
 
         [Required]
-        [FutureDate]
+        //[FutureDate]
         public string Date { get; set; }
 
         [Required]
